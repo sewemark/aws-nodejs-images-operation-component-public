b4b4c6771dee29be4c940f3eaadb8773ac4847c0
sewemark <sewerynjakub.markowicz@gmail.com>
2017-08-14 11:03:08 +0200

    Recieving SQS added

--- app.js
+++ app.js
@@ -1,3 +1,36 @@
-/**
- * Created by sew on 2017-08-13.
- */
+var AWS = require('aws-sdk');
+
+AWS.config.loadFromPath('./config.json');
+
+var queueURL = "https://sqs.us-east-2.amazonaws.com/810664644484/sewemarkMessageQueue";
+
+var params = {
+    AttributeNames: [
+        "SentTimestamp"
+    ],
+    MaxNumberOfMessages: 1,
+    MessageAttributeNames: [
+        "All"
+    ],
+    QueueUrl: queueURL,
+    VisibilityTimeout: 0,
+    WaitTimeSeconds: 0
+};
+
+sqs.receiveMessage(params, function(err, data) {
+    if (err) {
+        console.log("Receive Error", err);
+    } else {
+        var deleteParams = {
+            QueueUrl: queueURL,
+            ReceiptHandle: data.Messages[0].ReceiptHandle
+        };
+        sqs.deleteMessage(deleteParams, function(err, data) {
+            if (err) {
+                console.log("Delete Error", err);
+            } else {
+                console.log("Message Deleted", data);
+            }
+        });
+    }
+});
