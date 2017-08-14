bf80eb407d362eb6853069b3accb2c563f38765a
sewemark <sewerynjakub.markowicz@gmail.com>
2017-08-14 12:14:37 +0200

    Queue address changed

--- app.js
+++ app.js
@@ -2,37 +2,29 @@ var AWS = require('aws-sdk');
 
 AWS.config.loadFromPath('./config.json');
 
-var queueURL = "https://sqs.us-east-2.amazonaws.com/810664644484/sewemarkMessageQueue";
+var  queueUrl= 'https://sqs.us-east-2.amazonaws.com/810664644484/image-operation';
 
 var params = {
-    AttributeNames: [
-        "SentTimestamp"
-    ],
-    MaxNumberOfMessages: 1,
-    MessageAttributeNames: [
-        "All"
-    ],
-    QueueUrl: queueURL,
-    VisibilityTimeout: 0,
-    WaitTimeSeconds: 0
+    QueueUrl: queueUrl,
 };
 
-var sqs = new AWS.SQS({apiVersion: '2012-11-05'});
+var sqs = new AWS.SQS();
 
 sqs.receiveMessage(params, function(err, data) {
     if (err) {
         console.log("Receive Error", err);
     } else {
+        console.log(data);
         var deleteParams = {
             QueueUrl: queueURL,
             ReceiptHandle: data.Messages[0].ReceiptHandle
         };
-        sqs.deleteMessage(deleteParams, function(err, data) {
+        /*sqs.deleteMessage(deleteParams, function(err, data) {
             if (err) {
                 console.log("Delete Error", err);
             } else {
                 console.log("Message Deleted", data);
             }
-        });
+        });*/
     }
 });
