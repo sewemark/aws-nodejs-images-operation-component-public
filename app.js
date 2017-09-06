4af8a9351b314239af714bb73764a1c96592ba34
sewemark <sewerynjakub.markowicz@gmail.com>
2017-09-06 15:24:48 +0200

    File saving path fix

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 var sharp= require('sharp');
 var AWS = require('aws-sdk');
 const fs = require('fs');
-//AWS.config.loadFromPath('./config.json');
-//AWS.config.loadFromPath('./package.json');
+
 AWS.config = new AWS.Config();
 
 AWS.config.accessKeyId = "AKIAJWMKWOHSHPFJ7XAQ";
@@ -13,87 +12,76 @@ var  queueUrl= 'https://sqs.us-east-2.amazonaws.com/810664644484/image-operation
 
 var params = {
     QueueUrl: queueUrl,
-    AttributeNames: [
-        "SentTimestamp"
-    ],
+    AttributeNames: ["SentTimestamp"],
     MaxNumberOfMessages: 1,
-    MessageAttributeNames: [
-        "All"
-    ],
+    MessageAttributeNames: ["All"],
     WaitTimeSeconds: 20
 };
 
-
 var sqs = new AWS.SQS();
-
-
- var rec = function () {
+var rec = function () {
 
   sqs.receiveMessage(params, function(err, data) {
         if (err) {
             console.log("Receive Error", err);
         }
 
-         console.log(data);
-            if(data.Messages) {
-                var body = data.Messages[0].Body;
-                var tempData = data;
-                var files = JSON.parse(body);
-                files.forEach(function (item) {
+        if(data.Messages) {
+            var body = data.Messages[0].Body;
+            var tempData = data;
+            var item = JSON.parse(body);
 
-                    var params = {Bucket: 'sewemarkbucket', Key: item.file};
-                    s3.getObject(params, function (err, data) {
-                        if (err) console.log(err, err.stack); // an error occurred
-                        else {
-                            var trimmed =new Date().toDateString().trim();
-                            var path = trimmed + item.operation     + item.file;
-                            if (item.operation == "rotateRadio") {
-                                console.log(item.params.angels);
-                                console.log(path);
-                                sharp(data.Body)
-                                    .rotate(Number(item.params.angels))
-                                    .toFile(path, function (err, info) {
-                                        if (err) console.log(err);
-                                        else{ console.log(info);
-                                            save(path);
-                                        }
-                                    });
-                            } else if (item.operation == "resizeRadio") {
-                                sharp(data.Body)
-                                    .resize(item.params.width, item.params.height)
-                                    .toFile(item.file + item.operation + new Date().toDateString(), function (err, info) {
-                                        if (err) console.log(err);
-                                        else{
-                                            save(path);
-                                            console.log(info);
-                                        }
-                                    });
-                            }
+                var params = {Bucket: 'sewemarkbucket', Key: item.file};
+                s3.getObject(params, function (err, data) {
+                    if (err) console.log(err, err.stack);
+                    else {
+                        var trimmed =new Date().toDateString().trim();
+                        var path = trimmed + item.operation     + item.file;
 
-                        }
-
-                        var params = {
-                            QueueUrl: queueUrl, /* required */
-                            ReceiptHandle: tempData.Messages[0].ReceiptHandle /* required */
-                        };
-                        sqs.deleteMessage(params, function (err, data) {
-                            if (err) console.log(err, err.stack); // an error occurred
-                            else     console.log(data);           // successful response
+                        if (item.operation == "rotateRadio") {
+                            sharp(data.Body)
+                                .rotate(Number(item.params.angels))
+                                .toFile(path, function (err, info) {
+                                    if (err) console.log(err);
+                                    else{ console.log(info);
+                                        save(path);
+                                    }
                         });
+                        } else if (item.operation == "resizeRadio") {
+                            sharp(data.Body)
+                                .resize(Number(item.params.width), Number(item.params.height))
+                                .toFile(path, function (err, info) {
+                                    if (err) console.log(err);
+                                    else{
+                                        save(path);
+                                        console.log(info);
+                                    }
+                                });
+                        }
+                    }
+
+                    var params = {
+                        QueueUrl: queueUrl,
+                        ReceiptHandle: tempData.Messages[0].ReceiptHandle
+                    };
+
+                    sqs.deleteMessage(params, function (err, data) {
+                        if (err) console.log(err, err.stack);
+                        else     console.log(data);
                     });
-                })
-                setTimeout(function() {
-                    rec()
-                }, 60 * 1000);
+                });
 
-            }else{
-                setTimeout(function() {
-                    rec()
-                }, 60 * 1000);
-            }
+            setTimeout(function() {
+                rec()
+            }, 60 * 1000);
 
+        }else{
+            setTimeout(function() {
+                rec()
+            }, 60 * 1000);
+        }
     });
- }
+ };
 
  function save(path) {
      var params = {
